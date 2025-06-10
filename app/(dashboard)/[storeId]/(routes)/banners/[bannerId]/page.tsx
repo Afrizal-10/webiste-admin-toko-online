0ad17751bec28604554b606010bcf6fe100c59a4
afrizal-10 <adijuliyantoafrizal10@gmail.com>
2025-06-10 14:37:17 +0700

    fix bug

--- app/(dashboard)/[storeId]/(routes)/banners/[bannerId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/banners/[bannerId]/page.tsx
@@ -1,12 +1,22 @@
 import db from "@/lib/db";
 import {BannerForm} from "./components/banner-form";
+import {type ReadonlyURLSearchParams} from "next/navigation";
 
-const BannerPage = async ({params}: {params: {bannerId: string}}) => {
+interface PageParams {
+  params: {
+    storeId: string;
+    bannerId: string;
+  };
+  searchParams?: ReadonlyURLSearchParams;
+}
+
+const BannerPage = async ({params}: PageParams) => {
   const banner = await db.banner.findUnique({
     where: {
       id: params.bannerId,
     },
   });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
